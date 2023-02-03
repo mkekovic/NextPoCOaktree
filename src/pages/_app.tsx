d1f87243c9853ccb09a0269f048f489185e3c5fb
mkekovic <mkekovic@islander.tamucc.edu>
2023-02-03 09:24:56 -0600

    Fixed Session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,38 @@
 import { type AppType } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
 import { api } from "../utils/api";
 import "../styles/globals.css";
 import { PublicClientApplication, EventType } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
-import { SessionProvider } from "next-auth/react"
 
 import { Layout } from '../components/index';;
 
 
-const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps }, }) => {
-  const pca = new PublicClientApplication({
-    auth: {
-      clientId: 'daccbcbd-9465-4cbd-a810-439451287747',
-      authority: 'https://login.microsoftonline.com/997bb24e-f4ba-452f-8f21-13714e0a657b',
-      redirectUri: '/',
-    }
-  })
+const MyApp = ({ Component, pageProps, }: AppProps<{ session: Session; }>) => {
 
-  pca.addEventCallback(event => {
-    if (event.eventType === EventType.LOGIN_SUCCESS) {
-      console.log(event);
-      pca.setActiveAccount(event.payload.account)
-    }
-  })
+// const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps }, }) => {
+  // const pca = new PublicClientApplication({
+  //   auth: {
+  //     clientId: 'daccbcbd-9465-4cbd-a810-439451287747',
+  //     authority: 'https://login.microsoftonline.com/997bb24e-f4ba-452f-8f21-13714e0a657b',
+  //     redirectUri: '/',
+  //   }
+  // })
+
+  // pca.addEventCallback((event: any) => {
+  //   if (event.eventType === EventType.LOGIN_SUCCESS) {
+  //     console.log(event);
+  //     pca.setActiveAccount(event.payload.account)
+  //   }
+  // })
 
   return (
 
     // <MsalProvider instance={pca}>
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Layout>
         <Component {...pageProps} />
       </ Layout>
@@ -37,15 +41,5 @@ const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps }, }) =>
   );
 };
 
-// const MyApp = ({
-//   Component: any,
-//   pageProps: { session, ...pageProps },
-// }) => {
-//   return (
-//     <SessionProvider session={session}>
-//       <Component {...pageProps} />
-//     </SessionProvider>
-//   )
-// }
 
 export default api.withTRPC(MyApp);
