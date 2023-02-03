f2f7fa82eedb3d19c7921e11354c8ff3d2d1b661
mkekovic <mkekovic@islander.tamucc.edu>
2023-02-03 11:47:31 -0600

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,5 +17,16 @@ export const authOptions = {
         clientSecret: "8380945a3dbdaf9c2fada9dafbc4d4ea002bb8a9" //process.env.GITHUB_SECRET,
       }),
   ],
+  jwt: {
+    // A secret to use for key generation. Defaults to the top-level `secret`.
+   secret: process.env.NEXTAUTH_SECRET,
+    // The maximum age of the NextAuth.js issued JWT in seconds.
+    // Defaults to `session.maxAge`.
+    maxAge: 60 * 60 * 24 * 30,
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behavior.
+    // async encode({ secret, token, maxAge }) {},
+    // async decode({ secret, token }) {},
+  }
 }
 export default NextAuth(authOptions)
