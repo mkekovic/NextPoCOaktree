c3ecf620c2f544271643992cc9b898b94cb4468d
mkekovic <mkekovic@islander.tamucc.edu>
2023-02-04 12:24:45 -0600

    Update ImageCard.tsx

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,7 +3,7 @@ export default function ImageCard(props: any) {
         <div className="mx-1 my-3 border border-gray-300 max-w-sm rounded overflow-hidden shadow-lg">
             <img className=" w-full" src={props.src} key={props.src} alt="dog_images" />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <div className="font-bold text-xl mb-2">The Coolest Sunset</div>
                 <p className="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! eaque, exercitationem praesentium nihil.
                 </p>
